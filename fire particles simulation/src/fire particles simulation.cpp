d1840576f92e4433211176dd95f59f1b4fe441ae
vishnukumar <vishnulalmaddy@gmail.com>
2020-12-01 17:56:27 +0530

    Create particles

--- fire particles simulation/src/fire particles simulation.cpp
+++ fire particles simulation/src/fire particles simulation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <math.h>
 #include "Screen.h"
 
 using namespace std;
@@ -7,32 +8,40 @@ using namespace localnp;
 
 int main() {
 
-
 	Screen screen;
 
-	if(screen.init() == false) {
+	if (screen.init() == false) {
 		return 1;
 	}
 
-	for(int y=0; y< Screen::SCREEN_HEIGHT; y++) {
-		for(int x=0; x< Screen::SCREEN_WIDTH; x++) {
-			screen.setPixel(x, y, 128, 0, 255);
-		}
-
-	}
-
-	screen.update();
-
+	// Look for messages/events
 	bool quit = false;
 
 	while (!quit) {
-		if(screen.processEvents() == false){
+
+		int elapsed = SDL_GetTicks();
+
+		unsigned char red = (unsigned char) ((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned char green =
+				(unsigned char) ((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned char blue = (unsigned char) ((1 + sin(elapsed * 0.0003)) * 128);
+
+		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
+			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
+				screen.setPixel(x, y, red, green, blue);
+			}
+
+		}
+
+		// Draw the screen
+		screen.update();
+
+		if (screen.processEvents() == false) {
 			break;
 		}
 	}
 
 	screen.close();
-
 
 	return 0;
 }
